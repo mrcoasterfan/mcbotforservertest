5cc9a63eb764f5f421a744cb77d0c2745a385f99
mrcoasterfan <36242772+mrcoasterfan@users.noreply.github.com>
2018-02-07 20:13:02 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const token = "NDEwODY0OTIwMjIxOTc0NTI4.DVzXdw.S0lq_2k6M9cDUMdVB5QlOJYAtGY";
+var prefix = ".";
+var mention = "<@410864920221974528>";
+var memberCount = client.users.size;
+var servercount = client.guilds.size;
+
+client.on("ready", () => {
+	var servers = client.guilds.array().map(g => g.name).join(',');
+	console.log("--------------------------------------");
+console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
+});
+
+client.on('message', message => {
+	if (message.content === ("test")){
+	message.reply('test !');
+} else if (message.content === ("bonjour")){
+	message.reply('bonjour, je suis dingo, que puis-je faire pour vous ? Ho Ho Ho ');
+} else if(message.content.startsWith('!botname')){
+	client.user.setUsername(message.content.substr(9));
+} else if (message.content === "!musique") {
+	let m = " ";
+	m += 'Je ne peux pas encore le faire.. \n';
+	m += '';
+	m += ' \n';
+}
+});
+
+client.login(token)
